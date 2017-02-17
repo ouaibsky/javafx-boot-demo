a97d558d375cd3f91ea55daa30aaa182d087103e
Christophe <ouaibsky@gmail.com>
2017-02-18 00:58:47 +0100

    ADD COMBO INTO textfield

--- src/main/java/org/icroco/boot/javafx/MainPanePresenter.java
+++ src/main/java/org/icroco/boot/javafx/MainPanePresenter.java
@@ -1,12 +1,15 @@
 package org.icroco.boot.javafx;
 
 import de.felixroske.jfxsupport.FXMLController;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.geometry.Side;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
@@ -16,8 +19,6 @@ import org.controlsfx.control.PopOver;
 import org.controlsfx.control.PropertySheet;
 import org.controlsfx.control.PropertySheet.Item;
 import org.controlsfx.control.StatusBar;
-import org.controlsfx.glyphfont.FontAwesome;
-import org.controlsfx.glyphfont.Glyph;
 import org.controlsfx.property.BeanProperty;
 import org.icroco.boot.javafx.config.MyConfig;
 import org.icroco.boot.javafx.pref.UserPref;
@@ -43,7 +44,7 @@ public class MainPanePresenter {
     private String name;
 
     @Inject
-            @Lazy
+    @Lazy
     UserPref userPref;
 
     @Inject
@@ -57,6 +58,9 @@ public class MainPanePresenter {
 
 	@FXML
 	StatusBar topStatusBar;
+
+    @FXML
+    StatusBar bottomStatusBar;
 
     @FXML
     HiddenSidesPane hiddenPane;
@@ -73,8 +77,15 @@ public class MainPanePresenter {
     @FXML
     public void initialize() {
         log.info("MainPanePresenter --> initialize");
-        final Label gear = new Label("", new Glyph("FontAwesome", FontAwesome.Glyph.GEAR));
-        final Label hamburger = new Label("", new Glyph("FontAwesome", FontAwesome.Glyph.BARS));
+        final FontAwesomeIconFactory fontAwesomeIconFactory = FontAwesomeIconFactory.get();
+        final Label gear = new Label("", fontAwesomeIconFactory.createIcon(FontAwesomeIcon.GEAR));
+        gear.setMaxHeight(Double.MAX_VALUE);
+
+        //final Label gear = new Label("", new Glyph("FontAwesome", FontAwesome.Glyph.GEAR));
+        //final Label hamburger = new Label("", new Glyph("FontAwesome", FontAwesome.Glyph.BARS));
+        final Label hamburger = new Label("", fontAwesomeIconFactory.createIcon(FontAwesomeIcon.BARS));
+        hamburger.setMaxHeight(Double.MAX_VALUE);
+
         Button test = new Button("test");
         test.setOnMouseClicked(e -> popOver.hide());
 
@@ -87,16 +98,26 @@ public class MainPanePresenter {
             propertySheet.getItems().setAll(list);
     //    });
 
-        propertySheet.setPrefSize(200, 200);
+        //propertySheet.setPrefSize(200, 200);
         popOver.setContentNode(propertySheet);
 
-        popOver.setArrowLocation(PopOver.ArrowLocation.TOP_RIGHT);
+        popOver.setArrowLocation(PopOver.ArrowLocation.RIGHT_TOP);
         gear.setOnMouseClicked(e -> popOver.show(gear));
         hiddenPane.setTriggerDistance(25);
         hamburger.setOnMouseClicked((MouseEvent e) -> hiddenPane.setPinnedSide(hiddenPane.getPinnedSide() != null ? null : Side.LEFT));
         topStatusBar.getRightItems().add(gear);
         topStatusBar.getLeftItems().add(hamburger);
-        mainScrollPane.setContent(underlyingView.getView());
+        findRequestOnClicked(null);
+        //mainScrollPane.setContent(underlyingView.getView());
+
+        Label taskLabel = new Label("no task to be executed");
+        ProgressIndicator indicator = new ProgressIndicator();
+        taskLabel.setMaxHeight(Double.MAX_VALUE);
+
+        bottomStatusBar.getRightItems().add(taskLabel);
+        indicator.setProgress(100);
+        indicator.setVisible(false);
+        bottomStatusBar.getRightItems().add(indicator);
     }
 
     protected final <T> T getTargetObject(Object proxy) throws Exception {
--- src/main/java/org/icroco/boot/javafx/req/RfqSearchController.java
+++ src/main/java/org/icroco/boot/javafx/req/RfqSearchController.java
@@ -1,12 +1,14 @@
 package org.icroco.boot.javafx.req;
 
 import de.felixroske.jfxsupport.FXMLController;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.paint.Color;
+import javafx.scene.control.ComboBox;
+import javafx.scene.text.Text;
 import org.controlsfx.control.textfield.CustomTextField;
-import org.controlsfx.glyphfont.FontAwesome;
-import org.controlsfx.glyphfont.Glyph;
 import org.controlsfx.glyphfont.GlyphFont;
 import org.controlsfx.glyphfont.GlyphFontRegistry;
 import org.icroco.boot.javafx.MainPane;
@@ -24,20 +26,28 @@ public class RfqSearchController {
     MainPane starter;
 
 	@FXML
-    CustomTextField bizIdTextField;
-    @FXML
-    CustomTextField uuidTextField;
-    @FXML
+    CustomTextField idTextField;
 
+    @FXML
     public void initialize() {
-        final Glyph value = fontAwesome.create(FontAwesome.Glyph.SEARCH).color(Color.LIGHTGRAY);
-        bizIdTextField.setRight(value);
+//        final Glyph value = fontAwesome.create(FontAwesome.Glyph.SEARCH).color(Color.LIGHTGRAY);
+        final Text value = FontAwesomeIconFactory.get().createIcon(FontAwesomeIcon.SEARCH); //.color(Color.LIGHTGRAY);
+        ComboBox<String> cb = new ComboBox<>(FXCollections.observableArrayList("Id", "Uuid"));
+        cb.setStyle("-fx-opacity: 1; -fx-text-fill: blue; -fx-background-color: transparent;    -fx-padding: 0 0 0 0;" +
+                "    -fx-border-insets: 0 0 0 0;" +
+                "    -fx-font: 12px \"Arial\" ; -fx-font-style: italic;");
+        //text4.setFont(Font.font("Arial", FontWeight.THIN, FontPosture.ITALIC, 16));
+        //cb.setMaxWidth(50);
+        cb.getSelectionModel().select(0);
 //        ComboBox<String> cb = new ComboBox<>(FXCollections.observableArrayList(
 //                "BizId",
 //                "UUID"
 //        ));
 //        bizIdTextField.setLeft(cb);
-        uuidTextField.setRight(fontAwesome.create(FontAwesome.Glyph.SEARCH).color(Color.LIGHTGRAY));
+        //uuidTextField.setRight(fontAwesome.create(FontAwesome.Glyph.SEARCH).color(Color.LIGHTGRAY));
+        idTextField.setRight(cb);
+        idTextField.setLeft(value);
+
     }
 
     public void clickMiddleButton(ActionEvent actionEvent) {
